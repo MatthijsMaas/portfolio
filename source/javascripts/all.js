06b45eb96cf58d5f726ea6c22e1a57c2fc6cc005
Matthijs Maas <Matthijs.M.Maas@gmail.com>
2016-05-24 17:53:26 +0200

    portfolio V2

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,10 @@
+//= require scroll-reveal
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+//
+// $(document).ready
+
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -9,10 +13,35 @@ $(function() {
       if (target.length) {
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 1000);
+        }, 1100);
         return false;
       }
     }
   });
 });
 
+$(function(){
+
+  $(".tab").on("click", function(e){
+    // Change active tab
+    $(".tab").removeClass("active");
+    $(this).addClass("active");
+
+    // Hide all tab-content (use class="hidden")
+    $(".tab-content").addClass("hidden");
+
+    // Show target tab-content (use class="hidden")
+    tabSelector = $(this).data("target");
+    $(tabSelector).removeClass("hidden");
+
+  });
+
+  window.sr = ScrollReveal({ duration: 1000 });
+  sr.reveal('.banner', 100);
+  sr.reveal('.welcome', 100);
+  sr.reveal('.bar', 100);
+  sr.reveal('.card', 100);
+  sr.reveal('.navbar', 100);
+  sr.reveal('.tab', 100);
+
+});
